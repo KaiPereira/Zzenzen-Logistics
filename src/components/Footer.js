c88e66453195e6cfebabba1a068e04e0e8528f40
KaiPereira <88850028+KaiPereira@users.noreply.github.com>
2022-09-07 15:45:02 -0700

    added link to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
             <div className="footerNavElements">
                 <a href="#home" className="footerNavElement">Home</a>
                 <a href="#about" className="footerNavElement">About</a>
-                <a href="https://amazon-na.fountain.com/amazon-delivery-service-partner/apply/delivery-associate-zenzen-logistics-canada-incorporated-dmw1-zlci" target="_blank" rel="noreferrer" className="footerNavElement">Apply Now</a>
+                <a href="https://amazon-na.fountain.com/amazon-delivery-service-partner/apply/delivery-associate-zenzen-logistics-canada-inc-dvv2-zlci" target="_blank" rel="noreferrer" className="footerNavElement">Apply Now</a>
             </div>
         </footer>
     )
