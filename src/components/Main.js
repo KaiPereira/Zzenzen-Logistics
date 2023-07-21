fbf128a586589eff561219d9ab3f0204cd42c80c
Kai Pereira <kaipereira2020@gmail.com>
2023-07-21 09:39:40 -0700

    backup

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,8 +7,8 @@ export default function Main() {
                 <img src="/decorationalSlant.svg" className="decorationalSlant" alt="decorational slant graphic"/>
                 <img src="/decorationalCircle.svg" alt="decorational circle graphic" className="decorationalCircle" />
                 <div className="mainContentAlign">
-                    <h1>Your Last Mile Delivery Service For Growth</h1>
-                    <p className="main-description">Our customers are our number one priority and we reverse engineer from that standpoint to ensure that we exceed your last mile delivery expectations.</p>
+                    <h1>We are an Exclusive Delivery Service Partner of Amazon Canada</h1>
+                    <p className="main-description">Our Employees are a big part of the customer base we serve and our customers are our most important asset and our number one priority.  We reverse engineer from that standpoint to ensure we exceed our customers last mile delivery expectations</p>
                     <a className="contactUsButtonHome" href="mailto:zenzenlogistics@gmail.com" target="_blank" rel="noreferrer">Contact Us</a>
                 </div>
                 <div className="mainImagesAlign">
@@ -20,7 +20,10 @@ export default function Main() {
                 <div className="aboutAlignContent">
                     <p className="aboutMiniHeader">About Us</p>
                     <h1>Working With the Most Customer Obsessed Company in the World Has It’s Benefits</h1>
-                    <p className="aboutDescription">Zzenzen Logistics Canada Logistics Canada was founded in 2021 with the sole objective to be a national leader in the Last Mile delivery industry. Our goal is to serve the residents of Greater Victoria with the highest quality of customer care and safety possible. At Zzenzen Logistics, our motto is "Details Matter". Within our company we promote the ideals of accountability and service. </p>
+                    <p className="aboutDescription">
+                        Zzenzen Logistics Canada was founded in 2021 with the objective of to a national leader in the Last Mile Delivery Industry.
+                        <br /><br />
+                        Our goal is to serve our customers with the highest quality of customer care and safety possible. In addition, we strive to be the best corporate citizen that we can be by providing ample ongoing employment opportunities for the areas that we serve. We believe in a diverse and inclusive workforce and we are always looking for dynamic customer centric individuals to join our team!</p>
                     <a href="mailto:zenzenlogistics@gmail.com" target="_blank" rel="noreferrer">
                         <p>Contact Us</p>
                     </a>
@@ -58,6 +61,16 @@ export default function Main() {
                     <p>Join Us</p>
                 </a>
             </div>
+            <div className="gallery-align">
+                <section className="gallery">
+                    <img src="/gallery/gallery_4.jpg" alt="Gallery Image" />
+                    <img src="/gallery/gallery_2.jpg" alt="Gallery Image" />
+                    <img src="/gallery/gallery_3.jpg" alt="Gallery Image" />
+                    <img src="/gallery/gallery_1.png" alt="Gallery Image" />
+                    <img src="/gallery/gallery_5.jpg" alt="Gallery Image" />
+                    <img src="/gallery/gallery_6.jpg" alt="Gallery Image" />
+                </section>
+            </div>  
         </main>
     )
 }
